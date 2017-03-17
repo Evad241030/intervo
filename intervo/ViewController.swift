03226b71192b57d241c89540f65bd2b38bbe14e2
David Gonzalez <davismony@me.com>
2017-03-17 14:36:10 -0600

    Main timer is functional. Start / Stop button toggles.

--- intervo/ViewController.swift
+++ intervo/ViewController.swift
@@ -15,11 +15,12 @@ class ViewController: UIViewController {
     var framesShot: Int = 0
     var timerIsOn = false
     var clockOne = ClockReadout()
+    var startStopWatch: Bool = true
 
     // I'm thinking I'm might need this to increment.
     var fractions: Int = 0
     
-    
+    //TODO: I might need a quick reset button for the startStop Timer.
     
     //TODO: Add Outlet for segment Control
     
@@ -29,36 +30,31 @@ class ViewController: UIViewController {
     //TODO: Add Label Outet for Frames Counter
     
     //TODO: Add Outlet for Start Timer
-    
     @IBOutlet weak var timerLabel: UILabel!
     
     
     @IBOutlet weak var timerButton: UIButton!
     
     
-    //TODO: Add Action for Start Timer
-    
+    //This is essential my StartStop Button. I didn't rename it to avoid the hassle.
 
     @IBAction func timerButtonHit(_ sender: Any) {
         
-        if timerIsOn {
-
-            timer.invalidate()
-            timerButton.titleLabel?.text = "Stop"
-            timerIsOn = false
-
-        } else {
-            
+        if startStopWatch == true {
             timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(ViewController.updateTimer)), userInfo: nil, repeats: true)
-            timerIsOn = true
- 
+            startStopWatch = false
+            timerButton.setTitle("Stop Timer", for: UIControlState.normal)
+            
+        } else {
+            timer.invalidate()
+            timerButton.setTitle("Start Timer", for: UIControlState.normal)
+            startStopWatch = true
         }
         
     }
-
+//TODO: I want to add a "0" suffic for any time the second, minute, or hour is between 1-9... In the case of "blank" I want those to readout "00" 
     //MARK: This updates my first clock readout.
     func updateTimer() {
-
         clockOne.seconds += 1
         timerLabel.text = ("test 00:00:\(clockOne.seconds)")
         
